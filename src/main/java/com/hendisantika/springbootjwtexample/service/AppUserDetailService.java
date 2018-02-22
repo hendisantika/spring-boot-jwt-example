4939bf68dbba697c4eadd783afd78a3da39d45fe
Hendi Santika <hendisantika@gmail.com>
2018-02-22 07:58:32 +0700

    #11 - Add User Detail Service

---
+++ src/main/java/com/hendisantika/springbootjwtexample/service/AppUserDetailService.java
@@ -0,0 +1,53 @@
+package com.hendisantika.springbootjwtexample.service;
+
+import com.hendisantika.springbootjwtexample.entity.User;
+import com.hendisantika.springbootjwtexample.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-jwt-example
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 22/02/18
+ * Time: 07.58
+ * To change this template use File | Settings | File Templates.
+ */
+
+@Component
+public class AppUserDetailService implements UserDetailsService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String s) throws UsernameNotFoundException {
+
+        User user = userRepository.findByUsername(s);
+
+        if (user == null) {
+            throw new UsernameNotFoundException(String.format("username is invalid !"));
+        }
+
+
+        List<GrantedAuthority> authorities = new ArrayList<>();
+        user.getRoles().forEach(role -> {
+            authorities.add(new SimpleGrantedAuthority(role.getRoleName()));
+        });
+
+        UserDetails userDetails = new org.springframework.security
+                .core.userdetails.User(user.getUsername(), user.getPassword(), authorities);
+
+        return userDetails;
+    }
+}
