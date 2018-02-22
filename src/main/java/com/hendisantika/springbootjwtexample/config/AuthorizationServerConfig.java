ab81093248c8095cc9a1a02d8e6e0af4c2f4e6ce
Hendi Santika <hendisantika@gmail.com>
2018-02-23 06:17:35 +0700

    #13 - Add Authorization Server Config

---
+++ src/main/java/com/hendisantika/springbootjwtexample/config/AuthorizationServerConfig.java
@@ -0,0 +1,79 @@
+package com.hendisantika.springbootjwtexample.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
+import org.springframework.security.oauth2.provider.token.TokenEnhancerChain;
+import org.springframework.security.oauth2.provider.token.TokenStore;
+import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
+
+import java.util.Arrays;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-jwt-example
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 23/02/18
+ * Time: 06.17
+ * To change this template use File | Settings | File Templates.
+ */
+
+@Configuration
+@EnableAuthorizationServer
+public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdapter {
+
+    @Value("${security.jwt.client-id}")
+    private String clientId;
+
+    @Value("${security.jwt.client-secret}")
+    private String clientSecret;
+
+    @Value("${security.jwt.grant-type}")
+    private String grantType;
+
+    @Value("${security.jwt.scope-read}")
+    private String scopeRead;
+
+    @Value("${security.jwt.scope-write}")
+    private String scopeWrite;
+
+    @Value("${security.jwt.resource-ids}")
+    private String resourceIds;
+
+    @Autowired
+    private TokenStore tokenStore;
+
+    @Autowired
+    private JwtAccessTokenConverter accessTokenConverter;
+
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    @Override
+    public void configure(ClientDetailsServiceConfigurer configurer) throws Exception {
+        configurer
+                .inMemory()
+                .withClient(clientId)
+                .secret(clientSecret)
+                .authorizedGrantTypes(grantType)
+                .scopes(scopeRead, scopeWrite)
+                .resourceIds(resourceIds);
+    }
+
+    @Override
+    public void configure(AuthorizationServerEndpointsConfigurer endpoints) {
+        TokenEnhancerChain enhancerChain = new TokenEnhancerChain();
+        enhancerChain.setTokenEnhancers(Arrays.asList(accessTokenConverter));
+        endpoints.tokenStore(tokenStore)
+                .accessTokenConverter(accessTokenConverter)
+                .tokenEnhancer(enhancerChain)
+                .authenticationManager(authenticationManager);
+    }
+}
