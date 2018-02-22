5fddb56ca5b3d19769436a63cd9f0ff875dc99ea
Hendi Santika <hendisantika@gmail.com>
2018-02-22 07:41:59 +0700

    #1 - Initial Commit

---
+++ src/main/java/com/hendisantika/springbootjwtexample/SpringBootJwtExampleApplication.java
@@ -0,0 +1,12 @@
+package com.hendisantika.springbootjwtexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootJwtExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootJwtExampleApplication.class, args);
+    }
+}
