bd26b9720c456f238216c77e6f314e2bd999d2c6
Hendi Santika <hendisantika@gmail.com>
2018-02-23 06:18:46 +0700

    #14 - Add Resource Server Config

---
+++ src/main/java/com/hendisantika/springbootjwtexample/config/ResourceServerConfig.java
@@ -0,0 +1,47 @@
+package com.hendisantika.springbootjwtexample.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
+import org.springframework.security.oauth2.provider.token.ResourceServerTokenServices;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-jwt-example
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 23/02/18
+ * Time: 06.18
+ * To change this template use File | Settings | File Templates.
+ */
+
+@Configuration
+@EnableResourceServer
+public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
+
+    @Autowired
+    private ResourceServerTokenServices tokenServices;
+
+    @Value("${security.jwt.resource-ids}")
+    private String resourceIds;
+
+    @Override
+    public void configure(ResourceServerSecurityConfigurer resources) {
+        resources.resourceId(resourceIds).tokenServices(tokenServices);
+    }
+
+    @Override
+    public void configure(HttpSecurity http) throws Exception {
+        http
+                .requestMatchers()
+                .and()
+                .authorizeRequests()
+                .antMatchers("/actuator/**", "/api-docs/**").permitAll()
+                .antMatchers("/springjwt/**").authenticated();
+    }
+}
